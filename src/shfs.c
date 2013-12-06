ea3b4f209def055ba4d5e4e2cbf2aaf7c2026119
Jack <jhshanno@gwu.edu>
2013-12-06 15:12:56 -0800

    updated shfs

--- src/shfs.c
+++ src/shfs.c
@@ -1,387 +1,138 @@
+
 #define FUSE_USE_VERSION 26
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-
-#define _XOPEN_SOURCE 700
 #include <fuse.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <dirent.h>
 #include <errno.h>
-#include <sys/time.h>
-
-#ifdef HAVE_SETXATTR
-#include <sys/xattr.h>
-#endif
-
+#include <fcntl.h>
 #include "shadowTree.h"
 
-struct sh_state {
-  char *rootdir;
-};
+struct sf_root* root;
 
 static int sh_getattr(const char *path, struct stat *stbuf) {
-	int res;
-
-	res = lstat(path, stbuf);
-	if (res == -1)
-		return -errno;
-
-	return 0;
+    int res = 0;
+    struct user* user = find_user(root, path+1);
+    
+    printf("getattr\n");
+    memset(stbuf, 0, sizeof(struct stat));
+    
+    if (strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+	stbuf->st_nlink = 2;
+    } else if(user != NULL) {
+        stbuf->st_mode = S_IFDIR | 0755;
+	stbuf->st_nlink = 1;
+    } else {
+        stbuf->st_mode = S_IFREG | 0444;
+	stbuf->st_nlink = 1;
+	stbuf->st_size = 15;
+    }
+    //res = -ENOENT;
+    //NEED CASE FOR WRONG PATH INPUT
+    return res;
 }
-
-static int sh_access(const char *path, int mask) {
-	int res;
-
-	res = access(path, mask);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_readlink(const char *path, char *buf, size_t size) {
-	int res;
-
-	res = readlink(path, buf, size - 1);
-	if (res == -1)
-		return -errno;
-
-	buf[res] = '\0';
-	return 0;
-}
-
 
 static int sh_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi) {
-	DIR *dp;
-	struct dirent *de;
-
-	(void) offset;
-	(void) fi;
-
-	dp = opendir(path);
-	if (dp == NULL)
-		return -errno;
-
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
+			 off_t offset, struct fuse_file_info *fi) {
+    (void) offset;
+    (void) fi;
+    printf("readdir\n");
+    
+    struct user* user = root->head;
+    
+    if (strcmp(path, "/") == 0) {
+        filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	
+	while(user) {
+	  filler(buf, user->username, NULL, 0);
+	  user = user->next;
+	}
+    } else {
+        user = find_user(root, path+1);
+	if(user == NULL) {
+	    printf("user null\n");
+	    return -ENOENT;
 	}
 
-	closedir(dp);
-	return 0;
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	filler(buf, "hash", NULL, 0);
+	filler(buf, "days_since_changed", NULL, 0);
+	filler(buf, "days_until_can_change", NULL, 0);
+	filler(buf, "days_until_must_change", NULL, 0);
+	//finish user attr/info here
+    }
+    
+    return 0;
 }
-
-static int sh_mknod(const char *path, mode_t mode, dev_t rdev) {
-	int res;
-
-	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
-	   is more portable */
-	if (S_ISREG(mode)) {
-		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
-		if (res >= 0)
-			res = close(res);
-	} else if (S_ISFIFO(mode))
-		res = mkfifo(path, mode);
-	else
-		res = mknod(path, mode, rdev);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_mkdir(const char *path, mode_t mode) {
-	int res;
-	
-	res = mkdir(path, mode);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_unlink(const char *path) {
-	int res;
-
-	res = unlink(path);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_rmdir(const char *path) {
-	int res;
-
-	res = rmdir(path);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_symlink(const char *from, const char *to) {
-	int res;
-
-	res = symlink(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_rename(const char *from, const char *to) {
-	int res;
-
-	res = rename(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_link(const char *from, const char *to) {
-	int res;
-
-	res = link(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_chmod(const char *path, mode_t mode) {
-	int res;
-
-	res = chmod(path, mode);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_chown(const char *path, uid_t uid, gid_t gid) {
-	int res;
-
-	res = lchown(path, uid, gid);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_truncate(const char *path, off_t size) {
-	int res;
-
-	res = truncate(path, size);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-#ifdef HAVE_UTIMENSAT
-static int sh_utimens(const char *path, const struct timespec ts[2]) {
-	int res;
-
-	/* don't use utime/utimes since they follow symlinks */
-	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-#endif
 
 static int sh_open(const char *path, struct fuse_file_info *fi) {
-	int res;
+  printf("open\n");
+  /*if (strcmp(path, hello_path) != 0)
+		return -ENOENT;
 
-	res = open(path, fi->flags);
-	if (res == -1)
-		return -errno;
-
-	close(res);
+	if ((fi->flags & 3) != O_RDONLY)
+		return -EACCES;
+  */
 	return 0;
 }
 
 static int sh_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi) {
-	int fd;
-	int res;
+		      struct fuse_file_info *fi) {
+    printf("read\n");
+    size_t len;
+    (void) fi;
+    printf("path = %s\n\n", path);
+    struct user* user = find_user(root, path+1);
+    char uname[9];
+    
+    if(user != NULL || strcmp(path, "/") == 0)
+        return -ENOENT;
+    
+    int i;
+    for(i=1; i<10; i++) {
+        if(path[i] == '/') break;
+	uname[i-1] = path[i];
+    }
+    uname[i-1] = '\0';
+    user = find_user(root, uname);
+    i++;
+    
+    if(user == NULL)
+        return -ENOENT;
+    
+    printf("%s\n", user->hash);
 
-	(void) fi;
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
+    if(strcmp(path+i, "hash") == 0) {
+        memcpy(buf, user->hash + offset, size);
+    }
+    else if(strcmp(path+i, "days_since_changed") == 0) {
+        char s[10];
+	sprintf(s, "%d\n", user->dsc);
+	memcpy(buf, s + offset, sizeof(int));
+    }
+    
+    return size;
 }
-
-static int sh_write(const char *path, const char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi) {
-	int fd;
-	int res;
-
-	(void) fi;
-	fd = open(path, O_WRONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pwrite(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
-}
-
-static int sh_statfs(const char *path, struct statvfs *stbuf) {
-	int res;
-
-	res = statvfs(path, stbuf);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int sh_release(const char *path, struct fuse_file_info *fi) {
-	/* Just a stub.	 This method is optional and can safely be left
-	   unimplemented */
-
-	(void) path;
-	(void) fi;
-	return 0;
-}
-
-static int sh_fsync(const char *path, int isdatasync,
-		     struct fuse_file_info *fi) {
-	/* Just a stub.	 This method is optional and can safely be left
-	   unimplemented */
-  
-	(void) path;
-	(void) isdatasync;
-	(void) fi;
-	return 0;
-}
-
-#ifdef HAVE_POSIX_FALLOCATE
-static int sh_fallocate(const char *path, int mode,
-			off_t offset, off_t length,
-			struct fuse_file_info *fi) {
-	int fd;
-	int res;
-
-	(void) fi;
-
-	if (mode)
-		return -EOPNOTSUPP;
-
-	fd = open(path, O_WRONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = -posix_fallocate(fd, offset, length);
-
-	close(fd);
-	return res;
-}
-#endif
-
-#ifdef HAVE_SETXATTR
-/* xattr operations are optional and can safely be left unimplemented */
-static int sh_setxattr(const char *path, const char *name, const char *value,
-			size_t size, int flags) {
-	int res = lsetxattr(path, name, value, size, flags);
-	if (res == -1)
-		return -errno;
-	return 0;
-}
-
-static int sh_getxattr(const char *path, const char *name, char *value,
-			size_t size) {
-	int res = lgetxattr(path, name, value, size);
-	if (res == -1)
-		return -errno;
-	return res;
-}
-
-static int sh_listxattr(const char *path, char *list, size_t size) {
-	int res = llistxattr(path, list, size);
-	if (res == -1)
-		return -errno;
-	return res;
-}
-
-static int sh_removexattr(const char *path, const char *name) {
-	int res = lremovexattr(path, name);
-	if (res == -1)
-		return -errno;
-	return 0;
-}
-#endif /* HAVE_SETXATTR */
 
 static struct fuse_operations sh_oper = {
-	.getattr	= sh_getattr,
-	.access		= sh_access,
-	.readlink	= sh_readlink,
-	.readdir	= sh_readdir,
-	.mknod		= sh_mknod,
-	.mkdir		= sh_mkdir,
-	.symlink	= sh_symlink,
-	.unlink		= sh_unlink,
-	.rmdir		= sh_rmdir,
-	.rename		= sh_rename,
-	.link		= sh_link,
-	.chmod		= sh_chmod,
-	.chown		= sh_chown,
-	.truncate	= sh_truncate,
-#ifdef HAVE_UTIMENSAT
-	.utimens	= sh_utimens,
-#endif
-	.open		= sh_open,
-	.read		= sh_read,
-	.write		= sh_write,
-	.statfs		= sh_statfs,
-	.release	= sh_release,
-	.fsync		= sh_fsync,
-#ifdef HAVE_POSIX_FALLOCATE
-	.fallocate	= sh_fallocate,
-#endif
-#ifdef HAVE_SETXATTR
-	.setxattr	= sh_setxattr,
-	.getxattr	= sh_getxattr,
-	.listxattr	= sh_listxattr,
-	.removexattr	= sh_removexattr,
-#endif
+  .getattr	= sh_getattr,
+  .readdir	= sh_readdir,
+  .open		= sh_open,
+  .read		= sh_read,
 };
 
+
+
 int main(int argc, char *argv[]) {
-  struct sh_state* state = malloc(sizeof(struct sh_state));
-  umask(0);
+  root = sf_tree_init("blank");
   
-  state->rootdir = realpath(argv[argc-2], NULL);
-  argv[argc-2] = argv[argc-1];
-  argv[argc-1] = NULL;
-  argc--;
+  int a = sf_tree_add_user(root, "jack");
+  strcpy(root->head->hash, "test");
+  root->head->dsc = 5;
   
-  return fuse_main(argc, argv, &sh_oper, state);
+  return fuse_main(argc, argv, &sh_oper, NULL);
 }
