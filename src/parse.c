aff0608918223312f07467d2cfc8fea4d0ee04ea
chacunlesena <bmcgraw@gwmail.gwu.edu>
2013-12-06 18:34:01 -0500

    Parser testing file

---
+++ src/parse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "shadowTree.h"
+#include "parse.h"
+
+int main(){
+  struct sf_root *sf;
+  sf = init_parse("shadow.test", "/shadow/");
+  printf("First user: %s\n", sf->head->username);
+  printf("Second user: %s\n", sf->head->next->username);
+}
